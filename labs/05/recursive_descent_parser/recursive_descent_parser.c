04b5988162c679a074fe84ffff00710b96f13f32
JuanMelendres <juan.mele97@gmail.com>
2020-10-07 20:10:05 -0500

    A00369017-homework-04
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ labs/05/recursive_descent_parser/recursive_descent_parser.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/***************************
+Example:
+Grammar:
+E -> T | T  + E
+T -> int | int * T| ( E ) 
+***************************/
+
+char l;
+
+bool E();
+bool E_alpha();
+
+bool T();
+bool T_alpha_1();
+bool T_alpha_2();
+bool T_alpha_3();
+
+void error(){
+	printf("Error\n");
+	exit(-1);
+}
+
+// Match function
+bool match(char t) {
+    if (l == t) {
+        l = getchar();
+		return true;
+    }
+    else {
+		error();
+    }
+}
+
+// Definition of T' as per the given production
+bool T_alpha_1() {
+    bool aux;
+    if (aux == match('i') && match('n') && match('t')) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Definition of T' as per the given production
+bool T_alpha_2() {
+    if (match('*') && T()) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Definition of T' as per the given production
+bool T_alpha_3() {
+    bool aux;
+    if (match('(') && E() && match(')')) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Definition of T as per the given production
+bool T() {
+    if (l == '(') {
+        if (T_alpha_3()) {
+            return true;
+        }
+        else {
+            return false;
+        }    
+    }
+    else {
+        if (T_alpha_1()) {
+            if (l == '*') {
+                if (T_alpha_2()) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+        return false;
+    }
+}
+
+// Definition of E as per the given production
+bool E_alpha() {
+    if (T()) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Definition of E, as per the given production
+bool E() {
+    if (E_alpha()) {
+        if (l == '+') {
+            if (match('+') && E()) {
+                return true;
+            }
+            else {
+                return false;
+                error();
+            }
+        }
+        return true;
+    }
+    else {
+        error();
+    }
+}
+
+int main() {
+    do {
+        l = getchar();
+		// E is a start symbol.
+	    E();
+    } while (l != '\n' && l != EOF);
+    if (l == '\n') {
+        printf("Parsing Successful\n");
+    }
+}
